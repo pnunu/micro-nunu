55ab48eb1da61983f5cf2aa9794d7845a1f67553
pnunu <398269786@qq.com>
2018-08-08 15:21:25 +0800

    用户服务

---
+++ user-thrift-service/src/main/java/pnunu/user/mapper/UserMapper.java
@@ -0,0 +1,27 @@
+package pnunu.user.mapper;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import pnunu.thrift.user.UserInfo;
+
+/**
+ * @Author: pnunu
+ * @Date: Created in 15:07 2018/8/8
+ * @Description:
+ */
+@Mapper
+public interface UserMapper {
+    @Select("select id, username, password, real_name as realName" +
+            "mobile, email from pe_user where id = #{id}")
+    UserInfo getUserById(@Param("id")int id);
+
+    @Select("select id, username, password, real_name as realName" +
+            "mobile, email from pe_user where username = #{username}")
+    UserInfo getUserByName(@Param("username")String username);
+
+    @Insert("insert into pe_user (username, password, real_name, mobile, email)" +
+            "values (#{u.username},#{u.password},#{u.realName},#{u.mobile},#{u.email})")
+    void registerUser(@Param("u") UserInfo userInfo);
+}
--- user-thrift-service/src/main/java/pnunu/user/service/UserServiceImpl.java
+++ user-thrift-service/src/main/java/pnunu/user/service/UserServiceImpl.java
@@ -1,27 +1,33 @@
 package pnunu.user.service;
 
 import org.apache.thrift.TException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import pnunu.thrift.user.UserInfo;
 import pnunu.thrift.user.UserService;
+import pnunu.user.mapper.UserMapper;
 
 /**
  * @Author: pnunu
  * @Date: Created in 10:57 2018/8/6
  * @Description:
  */
+@Service
 public class UserServiceImpl implements UserService.Iface {
+    @Autowired
+    private UserMapper userMapper;
     @Override
     public UserInfo getUserById(int id) throws TException {
-        return null;
+        return userMapper.getUserById(id);
     }
 
     @Override
     public UserInfo getUserByName(String username) throws TException {
-        return null;
+        return userMapper.getUserByName(username);
     }
 
     @Override
     public void regiserUser(UserInfo userInfo) throws TException {
-
+        userMapper.registerUser(userInfo);
     }
 }
